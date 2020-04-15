401797be7b179279a2a910a0d72ed347009c21eb
Omkar Kaul <omkar@xtra.co.nz>
2020-04-16 01:51:38 +1200

    refactored Stocky class so that price attributes are stored as an object field and added BrokenPageExpection call if price cannot be found

--- Stocky.py
+++ Stocky.py
@@ -4,8 +4,12 @@ from bs4 import BeautifulSoup as soup
 
 class Stocky:
     """Class containing central functionality for Stocky CLI"""
-    def __init__(self, url):
-        self.url = url
+    def __init__(self):
+        self.url = "https://finance.yahoo.com/quote/"
+        self.price_attributes = {
+            "element":"span",
+            "attrs":{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}
+        }
     
     def check_stock(self):
         try:
@@ -13,7 +17,7 @@ class Stocky:
                 
             http = urllib3.PoolManager()
             client = http.urlopen('GET', self.url+asset, redirect=False)
-
+        
             if client.status >= 500:
                 raise ServerException()
             elif client.status != 200:
@@ -27,7 +31,12 @@ class Stocky:
             exit()
         else:
             page_html = soup(client.data, 'html.parser')
-            print(page_html.prettify())
+            current_price = page_html.find(self.price_attributes['element'],self.price_attributes['attrs']).get_text()
+
+            if current_price:
+                print(f"{asset}'s price: ${current_price}")
+            else:
+                raise BrokenPageException()
             
 
     def __get_asset_from_user(self):
