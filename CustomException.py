531fd12b5ae9ad0aee49bbd682d04493a265cd1b
Omkar Kaul <omkar@xtra.co.nz>
2020-04-16 01:04:36 +1200

    added ServerException for if the yahoo finance website is down

--- CustomException.py
+++ CustomException.py
@@ -1,4 +1,9 @@
 class BadAssetException(Exception):
     """Raised when user inputs a bad asset search string"""
     def __init__(self):
-        self.message = "Asset wasn't found. Try again!\nTIP: Ensure you're searching the right market if the stock has a market prefix/suffix!"
+        self.message = "Asset wasn't found. Try again!\nTIP: Ensure you're searching the right market if the stock has a market prefix/suffix!\n"
+
+class ServerException(Exception):
+    """Raised when 500+ status code is received when making a request"""
+    def __init__(self):
+        self.message = "Sorry, the server is not available right now!\n Try again later.\n"
