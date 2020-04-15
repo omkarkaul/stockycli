b267571d19a22a2299dd914b138dcee0dc9c88a4
Omkar Kaul <omkar@xtra.co.nz>
2020-04-16 02:50:01 +1200

    added latest time functionality, abstracted getting latest price and latest time out of check_stock function -> into their own private functions

--- Stocky.py
+++ Stocky.py
@@ -10,6 +10,9 @@ class Stocky:
             "element":"span",
             "attrs":{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}
         }
+        self.time_attributes = {
+            "id":"qquote-market-notice"
+        }
     
     def check_stock(self):
         try:
@@ -31,13 +34,9 @@ class Stocky:
             exit()
         else:
             page_html = soup(client.data, 'html.parser')
-            current_price = page_html.find(self.price_attributes['element'],self.price_attributes['attrs']).get_text()
-
-            if current_price:
-                print(f"{asset}'s price: ${current_price}")
-            else:
-                raise BrokenPageException()
+            stock_data = self.__get_stock_data(page_html)
             
+            print(f"{asset}'s latest price: ${stock_data['price']}, {stock_data['time']}\n")
 
     def __get_asset_from_user(self):
         asset = None
@@ -51,3 +50,39 @@ class Stocky:
                 print('Try again!')
         
         return asset
+
+    def __get_stock_data(self, bs4_html_object):
+        price = self.__get_stock_price(bs4_html_object)
+        time = self.__get_last_updated_time(bs4_html_object)
+
+        return {
+            "price":price,
+            "time":time
+        }
+
+    def __get_stock_price(self, bs4_html_object):
+        try:
+            latest_price_element = bs4_html_object.find(self.price_attributes['element'],self.price_attributes['attrs'])
+
+            if not latest_price_element:
+                raise BrokenPageException('price')
+        except BrokenPageException as e:
+            print(f"Error finding the latest {e.arg}!")
+            print(e.message)
+            exit() # temporary early exit, figure out how to make parent Exception class soft exit or custom exit
+        else:
+            return latest_price_element.get_text()
+    
+    def __get_last_updated_time(self, bs4_html_object):
+        try:
+            current_time_container = bs4_html_object.find(id=self.time_attributes['id'])
+
+            if not current_time_container:
+                raise BrokenPageException('time')
+        except BrokenPageException as e:
+            print(f"Error finding the latest {e.arg}!")
+            print(e.message)
+            exit() # temporary early exit, figure out how to make parent Exception class soft exit or custom exit
+        else:
+            market_notice_string = current_time_container.contents[0].get_text()
+            return market_notice_string[0].lower() + market_notice_string[1:] # return new string with first letter lowercase
