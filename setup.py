98bfa6a0589f7c7c675ce0a5fc1c135ce6410a65
Omkar Kaul <omkar@xtra.co.nz>
2020-04-17 19:57:37 +1200

    adding setup file for building stockycli as a python module

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="stockycli",
+    version="1.0",
+    py_modules=["stockycli"],
+    include_package_data=True,
+    install_requires = [
+        "click",
+        "beautifulsoup4",
+        "urllib3",
+    ],
+    entry_points = """
+        [console_scripts]
+        stockycli=stockycli:cli
+    """,
+    )
